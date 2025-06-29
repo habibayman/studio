cd2efefa26d0d35f312598a802bdbb761d1c5f3f
habibayman <habibaayman2004@outlook.com>
2025-06-29 06:18:56 +0300

    fix(texteditor)[image]: adjust insertion modal to a popover

--- contentcuration/contentcuration/frontend/shared/views/TipTapEditor/TipTapEditor/components/EditorToolbar.vue
+++ contentcuration/contentcuration/frontend/shared/views/TipTapEditor/TipTapEditor/components/EditorToolbar.vue
@@ -113,7 +113,7 @@
         :title="tool.title"
         :icon="tool.icon"
         :is-active="tool.isActive"
-        @click="onToolClick(tool)"
+        @click="onToolClick(tool, $event)"
       />
     </div>
   </div>
@@ -149,9 +149,9 @@
         t,
       } = useToolbarActions(emit);
 
-      const onToolClick = tool => {
+      const onToolClick = (tool, event) => {
         if (tool.name === 'image') {
-          emit('insert-image');
+          emit('insert-image', event.currentTarget);
         } else {
           // For all other buttons, call their original handler
           tool.handler();

a3635a98af7488efa36f219d3d37c0a99c343cf0
habibayman <habibaayman2004@outlook.com>
2025-07-02 09:25:49 +0300

    test - implement link handling

--- contentcuration/contentcuration/frontend/shared/views/TipTapEditor/TipTapEditor/components/EditorToolbar.vue
+++ contentcuration/contentcuration/frontend/shared/views/TipTapEditor/TipTapEditor/components/EditorToolbar.vue
@@ -163,6 +163,8 @@
       const onToolClick = (tool, event) => {
         if (tool.name === 'image') {
           emit('insert-image', event.currentTarget);
+        } else if (tool.name === 'link') {
+          emit('insert-link');
         } else {
           // For all other buttons, call their original handler
           tool.handler();
